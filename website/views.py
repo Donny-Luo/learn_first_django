903ff54aaa701bfe3f59da3b40298150b6c2229c
KillerDrop <johnkaylou@hotmaiil.com>
2024-01-06 22:39:08 +0800

    m

--- website/views.py
+++ website/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'D:\Learn_Coding\Pythone_Project\l_django\django_crm\dcrm\website\templates\home.html', {})
